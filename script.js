211a48b4b2c553d08edb38bdeb05203d4ea17860
Christian Gerlach <christianrgerlach@gmail.com>
2020-12-16 16:45:00 -0800

    prettyfiy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $(".update").change(function(){
+    $(".update").change(function() {
         var sum = 0
 
         var vip = ($("#vip").val() === 'true')
@@ -19,60 +19,50 @@ $(document).ready(function(){
                 if (sum < 20) {
                     var additional = 20 - sum
                     sum += additional
-                }
-                else {
+                } else {
                     sum += 5
                 }
-            }
-            else if (percent >= .3 && percent < .6 ) {
+            } else if (percent >= .3 && percent < .6) {
                 if (sum < 30) {
                     var additional = 30 - sum
                     sum += additional
-                }
-                else {
+                } else {
                     sum += 10
                 }
-            }
-            else if (percent >= .6) {
+            } else if (percent >= .6) {
                 if (sum < 40) {
                     var additional = 40 - sum
                     sum += additional
-                }
-                else {
+                } else {
                     sum += 15
                 }
             }
         }
+        // If there is no productivity issue penalize less
         else {
-            if (percent >= .1 && percent <= .3) {
+            if (percent >= .1 && percent < .3) {
                 if (sum < 10) {
                     var additional = 10 - sum
                     sum += additional
-                }
-                else {
+                } else {
                     sum += 5
                 }
-            }
-            else if (percent > .3) {
+            } else if (percent >= .3) {
                 if (sum < 15) {
                     var additional = 20 - sum
                     sum += additional
-                }
-                else {
+                } else {
                     sum += 10
                 }
             }
         }
-
-        
 
         // Calc business impact
         if (bizfunc) {
             if (sum < 30) {
                 var additional = 30 - sum
                 sum += additional
-            }
-            else {
+            } else {
                 sum += 5
             }
         }
@@ -81,8 +71,7 @@ $(document).ready(function(){
             if (sum < 30) {
                 var additional = 30 - sum
                 sum += additional
-            }
-            else {
+            } else {
                 sum += 10
             }
         }
@@ -93,8 +82,7 @@ $(document).ready(function(){
             if (sum < 20) {
                 var additional = 20 - sum
                 sum += additional
-            }
-            else {
+            } else {
                 sum += 5
             }
         }
@@ -122,7 +110,7 @@ $(document).ready(function(){
             $("body").css("color", "black")
             $("#resultval").text("Priority 2/5, your score is: " + sum)
         }
-        if (sum >= 40 ) {
+        if (sum >= 40) {
             $("body").css("background-color", "red")
             $("body").css("color", "white")
             $("#resultval").text("Priority 1/5, your score is: " + sum)
